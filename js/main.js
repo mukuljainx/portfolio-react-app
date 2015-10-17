9bfe742e57746366597a7b278a7ee0855e3849df
Mukul jain <jainmukul1996@gmail.com>
2015-10-18 03:31:53 +0530

    v 0.1

--- js/main.js
+++ js/main.js
@@ -3,23 +3,22 @@ $(window).scroll(function() {
 
     if ($(this).scrollTop()>200)
      {
-        $('#img1').css("-webkit-transform" , "translate(-200%)");
-        $('#img1').css("-webkit-transition" , "1s ease-in");
-        // window.alert("");
+        $('.oxxx').css("-webkit-transform" , "translate(-100%)");
+        $('.oxxx').css("-webkit-transition" , "1s ease-in");
      }
      else
      {
-        $('#img1').css("-webkit-transition" , "1s ease-in");
-        $('#img1').css("-webkit-transform" , "translate(0,0)");
+        $('.oxxx').css("-webkit-transition" , "1s ease-in");
+        $('.oxxx').css("-webkit-transform" , "translate(0,0)");
         // window.alert("");
      }
 
      if($(this).scrollTop()>1000)
      {
-        $('#img2').fadeIn();
+        $('.oxxx2').fadeIn();
      }
      else{
-        $('#img2').fadeOut();
+        $('.oxxx2').fadeOut();
      }
 
  });
