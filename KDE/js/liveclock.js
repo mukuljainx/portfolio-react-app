09753262c0dbab5beafe7ab864ea29334cdf82bd
Mukul jain <jainmukul1996@gmail.com>
2015-10-20 23:02:01 +0530

    Home page | intialization

---
+++ KDE/js/liveclock.js
@@ -0,0 +1,13 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('liveclock').innerHTML =
+    h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
