3c598249202e9091e59881cf718c39115c83e433
jainmukul <jainmukul1996@gmail.com>
2015-10-25 05:46:24 +0530

    PLinth | Event page is dynamic now

--- plinth/events/js/main.js
+++ plinth/events/js/main.js
@@ -1,1 +1,72 @@
 $(".button-collapse").sideNav();
+$('select').material_select();
+
+$( "select" )
+	.change(function() {
+    	var str;
+    	$( "select option:selected" ).each(function() {
+    		str = $( this ).text();
+    	});
+    	if(str == "Robotics")
+    	{
+    		$('#Robotics').show();
+    		$('#Management').hide();
+    		$('#Coding').hide();
+    		$('#Quizzing').hide();
+    		$('#Astronomy').hide();
+    		$('#Literary').hide();
+
+    	};
+    	if(str == "Management")
+    	{
+    		$('#Robotics').hide();
+    		$('#Management').show();
+    		$('#Coding').hide();
+    		$('#Quizzing').hide();
+    		$('#Astronomy').hide();
+    		$('#Literary').hide();
+
+    	};
+    	if(str == "Coding")
+    	{
+    		$('#Robotics').hide();
+    		$('#Management').hide();
+    		$('#Coding').show();
+    		$('#Quizzing').hide();
+    		$('#Astronomy').hide();
+    		$('#Literary').hide();
+
+    	};
+    	if(str == "Quizzing")
+    	{
+    		$('#Robotics').hide();
+    		$('#Management').hide();
+    		$('#Coding').hide();
+    		$('#Quizzing').show();
+    		$('#Astronomy').hide();
+    		$('#Literary').hide();
+
+    	};
+    	if(str == "Astronomy")
+    	{
+    		$('#Robotics').hide();
+    		$('#Management').hide();
+    		$('#Coding').hide();
+    		$('#Quizzing').hide();
+    		$('#Astronomy').show();
+    		$('#Literary').hide();
+
+    	};
+    	if(str == "Literary")
+    	{
+    		$('#Robotics').hide();
+    		$('#Management').hide();
+    		$('#Coding').hide();
+    		$('#Quizzing').hide();
+    		$('#Astronomy').hide();
+    		$('#Literary').show();
+
+    	};
+
+	})
+.trigger( "change" );
