a93e0d69ff2165702a643fb1fc5b27823a3080a1
jainmukul <jainmukul1996@gmail.com>
2015-12-23 22:39:49 +0530

    team

--- plinth/js/team.js
+++ plinth/js/team.js
@@ -1,7 +1,7 @@
-$('#img1').mouseenter(function(){
-	$(this).attr('src','./media/images/rain.gif')
-});
+// $('#img1').mouseenter(function(){
+// 	$(this).attr('src','./media/images/rain.gif')
+// });
 
-$('#img1').mouseleave(function(){
-	$(this).attr('src','./media/images/e6.gif')
-});
+// $('#img1').mouseleave(function(){
+// 	$(this).attr('src','./media/images/e6.gif')
+// });
